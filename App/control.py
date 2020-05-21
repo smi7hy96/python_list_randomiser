28bbc4f525e335c9dcb9d4d49d11dc1e257e349c
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-21 17:43:49 +0100

    added main method

--- App/control.py
+++ App/control.py
@@ -1,20 +1,23 @@
 from input_methods import *
 from Person import *
 
+def main():
+    print("\nRANDOMISE LIST APP \n")
 
-print("\nRANDOMISE LIST APP \n")
-
-all_names = []
-people = filetest('names.txt')
-for keys, values in people.items():
-    all_names.append(keys)
+    all_names = []
+    people = filetest('names.txt')
+    for keys, values in people.items():
+        all_names.append(keys)
 
 
-final_groups = run(all_names)
-for i in range(len(final_groups)):  # loop through the parent list
-    print("\n GROUP " + str(i + 1))
-    for j in range(len(final_groups[i])):
-     print(people.get(final_groups[i][j]).getName() + ", ", end=" ")
-     # print out the child lists containing the group members
+    final_groups = run(all_names)
+    for i in range(len(final_groups)):  # loop through the parent list
+        print("\n GROUP " + str(i + 1))
+        for j in range(len(final_groups[i])):
+         print(people.get(final_groups[i][j]).getName() + ", ", end=" ")
+         # print out the child lists containing the group members
 
-print("\n")
+    print("\n")
+
+if __name__ == '__main__':
+    main()
